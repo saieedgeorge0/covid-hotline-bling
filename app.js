617cfc86118b77fa762505575d0ddaf7b9efb139
George Saieed <saieed.george0@gmail.com>
2020-03-28 11:40:20 -0400

    add zipcode validation

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const session = require('express-session');
 const router = express.Router();
 const getResults = require("./scraper");
 const { urlencoded } = require('body-parser');
+const { postcodeValidator, postcodeValidatorExists } = require('postcode-validator');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const app = express();
 app.use(urlencoded({ extended: false}));
 app.use(session({secret: 'anything-you-want-but-keep-secret'}));
+import { postcodeValidator, postcodeValidatorExists } from 'postcode-validator';
 
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -25,22 +27,17 @@ app.post('/sms', async function (req, res) {
   const smsCount = req.session.counter || 0;
   const respValues = req.session.respvalues || [];
 
-  let message = `What state do you live in?`;
+  let message = `Hi! What's your zipcode? Once you let us know, please wait - it can take a few moments to pull the data.`;
 
   if(smsCount == 1) {
-    respValues.push(req.body.Body);
-    message = `Which county within ${respValues[0]} do you currently reside?`;
-  }
-
-  if(smsCount == 2) {
-    respValues.push(req.body.Body);
-    message = `What's your zip code? Once you let us know, please wait - it can take a few moments to pull the data.`;
-  }
-
-  if(smsCount == 3) {
-    respValues.push(req.body.Body);
-    const result = await getResults(respValues[2]);
-    message = `Your nearest center is the ${result.name}. You can call them at this number: ${result.phone}, or email them/visit their website here: ${result.email}.`;
+    if (postcodeValidator(respValues[0], 'US')) {
+      respValues.push(req.body.Body);
+      const result = await getResults(respValues[0]);
+      message = `Your nearest center is the ${result.name}. You can call them at this number: ${result.phone}, or email them/visit their website here: ${result.email}.`;
+    } else {
+      message = `Please enter a valid zipcode.`
+      smsCount = smsCount - 1;
+    }
   }
 
   req.session.counter = smsCount + 1;
@@ -49,7 +46,7 @@ app.post('/sms', async function (req, res) {
   if(req.body.Body.toLowerCase() == `clear`) {
     req.session.respvalues = [];
     req.session.counter = 0;
-    message = "You have cleared your information"
+    message = "You have cleared your information."
   }
 
   const twiml = new MessagingResponse();
