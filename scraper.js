aafdd06213c6867be5430615efec5b995ce7bc4c
George Saieed <saieed.george0@gmail.com>
2020-03-28 01:22:33 -0400

    set nosandbox attribute on puppeteer

--- scraper.js
+++ scraper.js
@@ -12,7 +12,7 @@ const getResults = async (zipcode) => {
 
     console.log("fetching data");
     
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: true, args:['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto(siteUrl, {waitUntil: 'load', timeout: 0});
 
